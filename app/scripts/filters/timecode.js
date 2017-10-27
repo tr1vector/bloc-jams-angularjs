305b1f86c7ce9e0ce99604478db94e5e05612e57
Matthew Fricke <fricke9523@msn.com>
2017-10-27 06:00:18 -0600

    Assignment 11 Filters Complete

--- app/scripts/filters/timecode.js
+++ app/scripts/filters/timecode.js
@@ -6,24 +6,29 @@
 		* @param {number} seconds
 		*/
         return function(seconds) {
-        	var seconds = Number.parseFloat(seconds);
+         //    var seconds = Number.parseFloat(seconds);
 
-        	if (Number.isNaN(seconds)){
-        		return '-:--';
-        	}
-            var wholeSeconds = Math.floor(seconds);
-            var minutes = Math.floor(wholeSeconds / 60);
-            var remainingSeconds = wholeSeconds % 60;
+         // 	   if (Number.isNaN(seconds)){
+         // 		   return '-:--';
+         // 	   }
+         //    var wholeSeconds = Math.floor(seconds);
+         //    var minutes = Math.floor(wholeSeconds / 60);
+         //    var remainingSeconds = wholeSeconds % 60;
  
-            var output = minutes + ':';
+         //    var output = minutes + ':';
  
-            if (remainingSeconds < 10) {
-                 output += '0';   
-            }
+         //    if (remainingSeconds < 10) {
+         //         output += '0';   
+         //    }
  
-            output += remainingSeconds;
+         //    output += remainingSeconds;
 
-            return output;
+         //    return output;
+         	var output = buzz.toTimer(seconds);
+         	if(output === '--:--'){
+         		output = '00:00';
+         	}
+         	return output;
         };
     }
  
